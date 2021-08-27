0cb296fde46f31fdace76f60ece99ed7bb2a1364
Charles Williams <charlesclinton2003@gmail.com>
2021-08-27 01:34:06 +0100

    added some tests

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :rumbl, Rumbl.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "chali",
+  password: "henry2014",
   database: "rumbl_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
