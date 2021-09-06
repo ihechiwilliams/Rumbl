9a3b667a069292cab172bf3c5f80f7274aaf25ee
Charles Williams <charlesclinton2003@gmail.com>
2021-09-06 22:50:45 +0100

    implemented video watching functiionality

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -2,6 +2,7 @@ defmodule Rumbl.Multimedia.Video do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, Rumbl.Multimedia.Permalink, autogenerate: true}
   schema "videos" do
     field :description, :string
     field :title, :string
@@ -19,5 +20,19 @@ defmodule Rumbl.Multimedia.Video do
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+    |> slugify_title()
+  end
+
+  defp slugify_title(changeset) do
+    case fetch_change(changeset, :title) do
+      {:ok, new_title} -> put_change(changeset, :slug, slugify(new_title))
+      :error -> changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
