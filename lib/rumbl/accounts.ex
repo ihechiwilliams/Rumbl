beb0cf6a49e372ad6106d746f670a39a2136abcf
Charles Williams <charlesclinton2003@gmail.com>
2021-09-07 13:56:07 +0100

    added tracking ability to track user in the video channel

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -1,7 +1,5 @@
 defmodule Rumbl.Accounts do
-  @moduledoc """
-  The Accounts context.
-  """
+  import Ecto.Query
 
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
@@ -54,5 +52,9 @@ defmodule Rumbl.Accounts do
         Pbkdf2.no_user_verify()
         {:error, :not_found}
     end
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from(u in User, where: u.id in ^ids))
   end
 end
